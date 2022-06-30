48f111cc5f7b68a59ee293f51ce69f32a1978027
Gangothri <gangothrigadige985@gmail.com>
2022-06-30 20:40:44 +0530

    added add method

--- src/main/java/com/tw/ArithmeticOperation.java
+++ src/main/java/com/tw/ArithmeticOperation.java
@@ -1,4 +1,12 @@
 package com.tw;
-
 public class ArithmeticOperation {
+    int augend;
+    int addend;
+    public ArithmeticOperation(int augend,int addend){
+        this.augend=augend;
+        this.addend=addend;
+    }
+    public int add(){
+        return 0;
+    }
 }
--- src/test/java/com/tw/ArithmeticOperationTest.java
+++ src/test/java/com/tw/ArithmeticOperationTest.java
@@ -1,4 +1,21 @@
 package com.tw;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ArithmeticOperationTest {
+
+    @Test
+    void shouldReturnSixWhenTwoAndFourAreAdded(){
+        ArithmeticOperation arithmeticOperation = new ArithmeticOperation(2,4);
+        int expectedSum=6;
+
+        int actualSum = arithmeticOperation.add();
+
+        assertEquals(expectedSum,actualSum);
+    }
+
+
+
 }
